eb1ae88fcd1e2d7d7c8206986ea961199e7324f1
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2024-05-08 21:05:56 +0530

    Add files via upload

---
+++ Assignment1/Assignment7.py
@@ -0,0 +1,25 @@
+iNo1=int(input("Enter first number :"))
+iNo2 = int(input("Enter second number :"))
+
+addition = iNo1+iNo2
+
+print("Addition of ",iNo1,"+",iNo2,'=',(addition))
+
+substraction=iNo1-iNo2
+
+print("Substraction of ",iNo1,"-",iNo2,'=',substraction)
+
+multiplication=iNo1*iNo2
+print("Multiplication",iNo1,"*",iNo2,'=',multiplication)
+
+division=iNo1/iNo2
+print("Division",iNo1,"/",iNo2,'=',division)
+
+power=iNo1**iNo2
+print(iNo1,'Power of',iNo2,'=',power)
+
+fd=iNo1//iNo2
+print("Floor division of ",iNo1,'//',iNo2,'=',fd)
+
+mod=iNo1%iNo2
+print("Modulos of ",iNo1,'%',iNo2,'=',mod)
